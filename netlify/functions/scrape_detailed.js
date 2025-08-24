e88dfed0f564f85fe463aacc5c9a4da941f76c53
mfalatine <michaelfalatine@gmail.com>
2025-08-24 03:45:21 -0400

    x3

--- netlify/functions/scrape_detailed.js
+++ netlify/functions/scrape_detailed.js
@@ -90,9 +90,14 @@ function extractCalendarStates(html) {
   console.log('Starting JSON parse...');
   
   try {
-    // Clean up the JSON string - remove trailing commas
+    // Clean up the JSON string - remove trailing commas and fix property names
     let cleanJson = jsonStr.replace(/,(\s*[}\]])/g, '$1');
-    console.log('JSON cleaned, attempting parse...');
+    
+    // Fix unquoted property names - this is the key fix!
+    cleanJson = cleanJson.replace(/(\w+):\s*([{\[])/g, '"$1": $2');
+    cleanJson = cleanJson.replace(/(\w+):\s*([^{\[\s][^,}\]]*)/g, '"$1": $2');
+    
+    console.log('JSON cleaned and property names fixed, attempting parse...');
     
     const data = JSON.parse(cleanJson);
     console.log('SUCCESS! Parsed JSON with', data.days ? data.days.length : 0, 'days');
